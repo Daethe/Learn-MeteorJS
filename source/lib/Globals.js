268ec8f88ed3548876128a47bae8e00d99fd892e
Marc Proux <marc.proux76@gmail.com>
2016-03-21 21:21:54 +0100

    Step 1

---
+++ source/lib/Globals.js
@@ -0,0 +1,5 @@
+Globals = {
+    appName: "OCShare",
+    appSlogan: "Un réseau social créé avec le cours d'OC",
+    schemas: {}
+};
---
+++ source/lib/Utils.js
@@ -0,0 +1,18 @@
+Utils = {
+    formatDate: function(date) {
+        var date = new Date(date);
+
+        var day = date.getDate().toString();
+        var month = (date.getMonth() + 1).toString();
+        var year = date.getFullYear();
+
+        if (day.length === 1)   {day   = '0' + day;  }
+        if (month.length === 1) {month = '0' + month;}
+
+        return day + '/' + month + '/' + year;
+    },
+
+    pathFor: function(routeName, params) {
+        return Router.routes[routeName].path(params);
+    }
+}
